7250ebda788f95a3b2465413b7d404f6c9cec83a
shakiblimon <shakibcselimon@gmail.com>
2017-04-14 21:41:07 +0600

    "  Displaying item after few time git with pychram  "

---
+++ delay.py
@@ -0,0 +1,15 @@
+import  time
+
+print("image1")
+time.sleep(3)
+print("image2")
+time.sleep(3)
+print("image3")
+'''
+for a in range(1,11):
+    print('\n')
+    time.sleep(2)
+    for b in range(1,11):
+        print(a, "x", b, "=", a * b)
+
+'''
