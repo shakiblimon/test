8af17c9b43f2f1696063809a3250094443f54bbe
shakiblimon <shakibcselimon@gmail.com>
2017-04-14 20:59:26 +0600

    after modifiting code

--- test_git.py
+++ test_git.py
@@ -1,1 +1,12 @@
-print (" thsis is test code for git ")
+print (" >>> This is test code for git <<< ")
+
+year = int(input("Enter the Year: "))
+
+if(year)%400 == 0:
+    print("This is leap year")
+elif (year)%4 == 0:
+    print("This year is leap year ")
+elif (year)%100 == 0:
+    print("THis is leap year ")
+else:
+    print("This is not leap year")
